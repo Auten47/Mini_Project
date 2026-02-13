3c0e0f538289b5aa35d44d26d9ef089ce874fb05
Nattachai-devlow <nattachai0046@gmail.com>
2026-02-14 05:19:10 +0700

    Update index.js

--- miniProject/frontend/src/index.js
+++ miniProject/frontend/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
+const clientIdGooGle = '96793548207-0pekgvjjddndhpm8c7puiis06igshmam.apps.googleusercontent.com';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId='96793548207-0pekgvjjddndhpm8c7puiis06igshmam.apps.googleusercontent.com'>
+    <GoogleOAuthProvider clientId={clientIdGooGle}>
       <App />
     </GoogleOAuthProvider>
   </React.StrictMode>
