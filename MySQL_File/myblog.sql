0ed037e1589e2bf4bf2c68c12ca2b3b5dc00cc09
Nattachai-devlow <nattachai0046@gmail.com>
2026-02-15 06:46:46 +0700

    Add files via upload

--- MySQL_File/myblog.sql
+++ MySQL_File/myblog.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 13, 2026 at 03:28 PM
+-- Generation Time: Feb 15, 2026 at 12:44 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -24,6 +24,86 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `comment_text` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `comments`
+--
+
+INSERT INTO `comments` (`id`, `post_id`, `user_id`, `comment_text`, `created_at`) VALUES
+(4, 3, 1, 'What ?', '2026-02-14 22:07:34'),
+(5, 3, 1, 'sdfsdf', '2026-02-14 22:08:14'),
+(7, 2, 1, 'Welcome', '2026-02-14 22:49:19'),
+(9, 4, 1, 'Hi', '2026-02-14 23:15:02'),
+(13, 6, 3, 'luv luv 😍', '2026-02-14 23:38:08');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `likes`
+--
+
+CREATE TABLE `likes` (
+  `id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `likes`
+--
+
+INSERT INTO `likes` (`id`, `post_id`, `user_id`, `created_at`) VALUES
+(3, 4, 2, '2026-02-14 22:47:16'),
+(4, 4, 3, '2026-02-14 22:48:05'),
+(5, 2, 1, '2026-02-14 23:06:18'),
+(7, 4, 1, '2026-02-14 23:14:28'),
+(9, 3, 1, '2026-02-14 23:20:48'),
+(10, 5, 1, '2026-02-14 23:26:20'),
+(11, 6, 2, '2026-02-14 23:37:19'),
+(12, 6, 3, '2026-02-14 23:38:03');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` text DEFAULT NULL,
+  `image` varchar(500) DEFAULT NULL,
+  `tag` varchar(100) DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`id`, `title`, `description`, `image`, `tag`, `user_id`, `created_at`) VALUES
+(1, 'test', 'test', '/uploads/1771090834550FilmCam_K Funs_20260126_012714844.jpg', 'Dialy Life', 1, '2026-02-14 17:40:34'),
+(2, 'Test23423', 'sdfsdfsdf', '/uploads/1771092537088IMG_20260126_223649.jpg', 'Dialy Life', 3, '2026-02-14 18:08:57'),
+(3, 'Music in life', 'I love music', '/uploads/1771096063674à¹à¸à¸µà¸¢à¸à¹.....png', 'Music', 1, '2026-02-14 19:07:43'),
+(4, 'Computer Engineer in 2026', 'How you think about cpe in 2026?', '/uploads/177109631039813304.jpg', 'Engineer', 1, '2026-02-14 19:11:50'),
+(5, 'Travel in bankok', 'I like ......', '/uploads/1771097602931FilmCam_Inst M_20260124_001116246.jpg', 'Travel', 1, '2026-02-14 19:33:23'),
+(6, 'Pictures from the past 🫰', 'Classmates from Kannasootsuksalai School, Class 120, Chinese Language Arts major.\r\n', '/uploads/1771112224869IMG_5333.JPG', 'Dialy Life', 2, '2026-02-14 23:37:04');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -40,11 +120,36 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `fullname`, `email`, `password`, `created_at`) VALUES
-(1, 'nattachai wongsrimueng', 'nattachai-01@gmail.com', 'nattachai', '2026-02-05 18:28:17');
+(1, 'nattachai wongsrimueng', 'nattachai-01@gmail.com', 'nattachai', '2026-02-05 18:28:17'),
+(2, 'Somchai Rakdee', 'somchai@gmail.com', 'somchai', '2026-02-13 16:35:08'),
+(3, '078-7 ณัฎฐชัย วงษ์ศรีเมือง', 'nattachai-wo@rmutp.ac.th', NULL, '2026-02-13 17:15:12');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `post_id` (`post_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `likes`
+--
+ALTER TABLE `likes`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `post_id` (`post_id`,`user_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `users`
@@ -58,10 +163,52 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `likes`
+--
+ALTER TABLE `likes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `likes`
+--
+ALTER TABLE `likes`
+  ADD CONSTRAINT `likes_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `likes_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `posts`
+--
+ALTER TABLE `posts`
+  ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
